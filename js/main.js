149460308290008f1f23e57209a121da6b43da75
Kirill Plyushchai <kirill.plyushchai@gmail.com>
2021-04-09 23:21:40 +0300

    add adaptive and connect mobile menu

--- js/main.js
+++ js/main.js
@@ -1,35 +1,112 @@
 const swiper = new Swiper(".chanel-slider", {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
 
     // Navigation arrows
     navigation: {
         nextEl: ".chanel-button-next",
         prevEl: ".chanel-button-prev",
     },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1400: {
+            slidesPerView: 6,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+        // when window width is >= 480px
+        1100: {
+            slidesPerView: 4,
+        },
+        // when window width is >= 640px
+        900: {
+            slidesPerView: 3,
+        },
+        740: {
+            slidesPerView: 2,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+    },
 });
 
 const recomendedSwiper = new Swiper(".recomended-slider", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 3,
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
 
-  // Navigation arrows
-  navigation: {
-      nextEl: ".recomended-button-next",
-      prevEl: ".recomended-button-prev",
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".recomended-button-next",
+        prevEl: ".recomended-button-prev",
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1400: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        870: {
+            slidesPerView: 2,
+        },
+    },
 });
 
 const foodSwiper = new Swiper(".food-slider", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 6,
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
 
-  // Navigation arrows
-  navigation: {
-      nextEl: ".food-button-next",
-      prevEl: ".food-button-prev",
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".food-button-next",
+        prevEl: ".food-button-prev",
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1400: {
+            slidesPerView: 6,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+        // when window width is >= 480px
+        1100: {
+            slidesPerView: 4,
+        },
+        // when window width is >= 640px
+        900: {
+            slidesPerView: 3,
+        },
+        760: {
+            slidesPerView: 2,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+    },
 });
+
+let searchButton = document.querySelector(".mobile-search");
+let mobileSearch = document.querySelector(".input-group");
+
+searchButton.addEventListener("click", () => {
+  mobileSearch.classList.toggle('is-open')
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  recomendedSwiper.destroy();
+  foodSwiper.destroy();
+}
